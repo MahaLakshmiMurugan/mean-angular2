be94a682846327ccce45a744b484b0c4627b849f
maha <maha@pearlthoughts.com>
2018-11-30 21:59:40 +0530

    Post listing and create

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Post } from '../post.model';
+import { Subscription } from 'rxjs';
+import { PostsService } from "../posts.service";
+
+@Component ({
+    selector: 'app-post-list',
+    templateUrl: './post-list.component.html',
+    styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy{
+/*@Input() posts: Post[] = [
+    {title: 'First Post', content: 'This is our first post'},
+    {title: 'Second Post', content: 'This is our second post'},
+    {title: 'Third Post', content: 'This is our third post'}
+]; */
+posts: Post[] = [];
+private postsSub: Subscription;
+
+constructor(public postsService: PostsService) {}
+
+ngOnInit() {
+    this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateLiscener()
+        .subscribe((posts: Post[])=> {
+            this.posts = posts;
+        })
+}
+
+ngOnDestroy() {
+    this.postsSub.unsubscribe();
+}
+}
