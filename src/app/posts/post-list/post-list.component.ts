3197037bc7483670965d35ff1bb31a1e3988f0e4
maha <maha@pearlthoughts.com>
2018-12-28 20:35:28 +0530

    implement pagination

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Post } from '../post.model';
 import { Subscription } from 'rxjs';
 import { PostsService } from "../posts.service";
+import { PageEvent } from "@angular/material";
 
 @Component ({
     selector: 'app-post-list',
@@ -16,23 +17,39 @@ export class PostListComponent implements OnInit, OnDestroy{
 ]; */
 posts: Post[] = [];
 isLoading = false;
+totalPosts = 0;
+postsPerPage = 2;
+currentPage = 1;
+pageSizeOptions = [1, 2, 5, 10];
 private postsSub: Subscription;
 
 constructor(public postsService: PostsService) {}
 
 ngOnInit() {
     this.isLoading = true;
-    this.postsService.getPosts();
-    this.postsSub = this.postsService.getPostUpdateLiscener()
-        .subscribe((posts: Post[])=> {
-            this.isLoading = false;
-            this.posts = posts;
-        })
-}
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
+    this.postsSub = this.postsService
+      .getPostUpdateListener()
+      .subscribe((postData: {posts: Post[], postCount: number}) => {
+        this.isLoading = false;
+        this.totalPosts = postData.postCount;
+        this.posts = postData.posts;
+      });
+  }
 
-OnDelete(postId: string) {
-    this.postsService.deletePost(postId);
-}
+  onChangedPage(pageData: PageEvent) {
+    this.isLoading = true;
+    this.currentPage = pageData.pageIndex + 1;
+    this.postsPerPage = pageData.pageSize;
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
+  }
+
+  onDelete(postId: string) {
+    this.isLoading = true;
+    this.postsService.deletePost(postId).subscribe(() => {
+      this.postsService.getPosts(this.postsPerPage, this.currentPage);
+    });
+  }
 
 ngOnDestroy() {
     this.postsSub.unsubscribe();
